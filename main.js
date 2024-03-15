67c5cdefa313d69806bbf595ae65ca9094ab545e
KiranRiaz57 <159231693+KiranRiaz57@users.noreply.github.com>
2024-03-15 22:32:56 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// Q19
+var guests = ["Kiran", "Mahrang", "Laiba", "Saira", "Benish"];
+console.log("I am inviting", guests.length, "people to dinner!");
+// Q20
+var countries = ["America", "Dubai", "turkey", "UK", "France"];
+console.log("Countries I'd like to visit:", countries);
+// Q21
+var book = {
+    title: "The Hobbit", author: "J.R.R. Tolkien", yearpublished: 1937
+};
+console.log("Book Info:", book.title, "by", book.author, "published in", book.yearpublished);
---
+++ main.ts
@@ -0,0 +1,13 @@
+// Q19
+let guests: string[] = ["Kiran","Mahrang","Laiba","Saira","Benish"]
+console.log("I am inviting",guests.length,"people to dinner!")
+// Q20
+let  countries: string[] = ["America","Dubai","turkey","UK","France"];
+console.log("Countries I'd like to visit:",countries)
+// Q21
+let book: {
+    title:string; author: string; yearpublished:number} =
+    {
+        title : "The Hobbit",author : "J.R.R. Tolkien", yearpublished:1937
+    }
+    console.log("Book Info:",book.title,"by",book.author,"published in",book.yearpublished)
